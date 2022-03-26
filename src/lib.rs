b59bf5f94b27b20efa1bd7612651844ca72f9f9f
HicaroD <hdanrlley1@gmail.com>
2022-03-26 19:07:32 -0300

    Lexer: Implementing parts of the lexer
    I'm considering everything as `Unknown` if it is not square brackets,
    equal sign or hash symbol, but I'll change that later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,42 @@
 #[derive(Debug)]
-struct Parser {}
+enum Token {
+    OpenSquareBracket,
+    ClosingSquareBracket,
+    Hash,
+    EqualSign,
+    Identifier(String),
+    Unknown,
+}
+
+struct Lexer {}
+
+impl Lexer {
+    fn tokenize(source_file: String) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+
+        for character in source_file.chars() {
+            match character {
+                '='   => tokens.push(Token::EqualSign),
+                '['   => tokens.push(Token::OpenSquareBracket),
+                ']'   => tokens.push(Token::ClosingSquareBracket),
+                token => tokens.push(Token::Unknown),
+            }
+        }
+
+        return tokens;
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let source_file = String::from("[something]\nname=hicaro\n");
+        let tokenized_file = Lexer::tokenize(source_file);
+        for token in tokenized_file.iter() {
+            println!("{:?}", token)
+        }
     }
 }
