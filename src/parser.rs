89b4f5146f148189b52e953c247dfecc3fd9562f
HicaroD <hdanrlley1@gmail.com>
2022-03-27 17:35:07 -0300

    Parser: Implemeting custom file read
    While I'm reading the file, I'm ignoring the line that starts with `;`
    which represents a comment line

--- src/parser.rs
+++ src/parser.rs
@@ -1,20 +1,38 @@
-use crate::lexer::*;
 use std::fs;
+use std::io::{self, BufRead, BufReader};
+use crate::lexer::*;
 
 pub struct Parser {}
 
 impl Parser {
-    pub fn parse<S: Into<String>>(file_path: S) {
-        let file = match fs::read_to_string(file_path.into()) {
+    fn read_file<S: Into<String>>(file_path: S) -> std::io::Result<String> {
+        let file = match fs::File::open(file_path.into()) {
             Ok(f) => f,
             Err(e) => {
-                eprintln!("Unable to read file. Error: {e}");
+                eprintln!("Unable to open file. Error: {e}");
                 std::process::exit(1);
             }
         };
-        let tokens = Lexer::tokenize(file);
+
+        let file_reader = BufReader::new(file);
+        let mut file_content = String::new();
+
+        for line in file_reader.lines() {
+            let line_content = line?;
+            if !line_content.starts_with(";") {
+                file_content.push_str(&line_content);
+            }
+        }
+        return Ok(file_content);
+    }
+
+    pub fn parse<S: Into<String>>(file_path: S) -> std::io::Result<()> {
+        let file_content = Parser::read_file(file_path)?;
+
+        let tokens = Lexer::tokenize(file_content);
         for token in tokens.iter() {
             println!("{:?}", token);
         }
+        Ok(())
     }
 }
